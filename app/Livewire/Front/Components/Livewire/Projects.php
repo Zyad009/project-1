0a2f2d7c27bfb3d241823b9d99bdf5966b76eb0a
zyad ashraf <zyad3300992@gmail.com>
2025-02-28 01:57:32 +0200

    small edit test

---
+++ app/Livewire/Front/Components/Livewire/Projects.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Front\Components\Livewire;
+
+use Livewire\Component;
+
+class Projects extends Component
+{
+    public function render()
+    {
+        return view('front.components.livewire.projects');
+    }
+}
